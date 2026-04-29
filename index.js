b2fc3a8f110dcb38e0ec1478e076eda18b4d13fe
randomdegenerate <sandilkosgahakumbura@gmail.com>
2026-04-29 10:08:08 +0100

    dummy initial stable version for devops/testing demonstrations

---
+++ index.js
@@ -0,0 +1,4 @@
+const maths = require("./maths.js")
+
+console.log(maths.multiply(3, 5))
+
---
+++ maths.js
@@ -0,0 +1,15 @@
+const PI = 3.14159;
+
+function multiply(a, b) {
+    return a * b
+}
+
+function subtract(a , b) {
+    return a - b
+}
+
+function sum(a , b) { 
+    return a + b
+}
+
+module.exports = {multiply, subtract , sum , PI}
